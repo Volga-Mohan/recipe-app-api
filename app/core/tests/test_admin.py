1bb1a0942b85eba5580f4186d5f23e74f3eda49f
Volga-Mohan <volgamohanan@gmail.com>
2020-12-22 17:31:30 +0530

    Added working list tags endpoint.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,8 +23,6 @@ class AdminSiteTests(TestCase):
         """Test that users are listed on the user page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
-
-
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
@@ -33,7 +31,7 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
-        
+
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
