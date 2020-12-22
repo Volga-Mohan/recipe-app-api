7a18275904c8ab2397c4c894fc48eee361bccf86
Volga-Mohan <volgamohanan@gmail.com>
2020-12-22 15:59:59 +0530

    Added manage user endpoint.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -24,6 +24,7 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
+
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
@@ -32,6 +33,7 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
+        
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
